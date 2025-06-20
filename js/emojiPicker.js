eb6cea72d7ff285c3d0c43079338bb50d27d7047
Kruzlath <boondartaki@gmail.com>
2025-06-20 12:01:28 +0300

    test

--- js/emojiPicker.js
+++ js/emojiPicker.js
@@ -1,23 +1,22 @@
- (function() {
+(function() {
   'use strict';
-
-  // a simple await，until #chat-room-bot loaded
+  
   function onReady(selector, fn) {
     const el = document.querySelector(selector);
     if (el) return fn(el);
     setTimeout(() => onReady(selector, fn), 200);
   }
 
-  onReady('#chat-room-bot', (container) => {
-    // 1. get the container and make sure it uses relative positioning
-    // if (getComputedStyle(container).position === 'static') {
-    //   container.style.position = 'relative';
-    // }
-    console.log("Guess who managed to make a custom BC extension!! THaT'S RIGHT!!!")
-    Player.Description="Testing If Description can be changed"
-    ServerPlayerSync()
+  function myPeriodicTask() {
+    console.log("Executing every 5 seconds");
+    // Your repeating code here
+  }
 
+  onReady('#chat-room-bot', (container) => {
+    // Run immediately when ready
+    myPeriodicTask();
     
-    
+    // Then every 5 seconds
+    setInterval(myPeriodicTask, 5000);
   });
 })();
